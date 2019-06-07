feaedf8977b22c4a28d1808a5e6ca052c2330f11
robinhanstuy <rhan@stuy.edu>
2019-06-07 00:54:15 -0400

    >

--- ConnectMe/__init__.py
+++ ConnectMe/__init__.py
@@ -13,7 +13,7 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 database.createdb()
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+app.secret_key = os.urandom(32)
 user = None
 
 def setUser(userName):
